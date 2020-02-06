d0ccd50f8a9913ed076ded5c01e56d777e15fd59
Quinn <32716387+Quazmoz@users.noreply.github.com>
2020-02-06 21:11:16 +0100

    Add files via upload

--- Restore folder permissions.ps1
+++ Restore folder permissions.ps1
@@ -12,11 +12,46 @@ Foreach ($name in $foldernames) {
     
     $acl.SetAccessRule($AccessRule)
 
+    $acl.SetAccessRuleProtection($False,$True)
+
     $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("CARMEUSE\IT Help Desk","FullControl","Allow")
+    
+    $acl.SetAccessRule($AccessRule)
+
+    $acl.SetAccessRuleProtection($False,$True)
+
+    $first,$last = $name -split " "
+    $name = "$last $first"
+    $acl | Set-Acl "..\$name"
+}
+
+#without helpdesk
+Foreach ($name in $foldernames) {
+    $acl = Get-Acl "..\$name"
+    $first,$last = $name -split " "
+    $name = "$last $first"
+    $user = Get-ADUser -Filter "Name -eq '$name'" -Properties SamAccountName
+
+    $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("CARMEUSE\$($user.SamAccountName)",'ContainerInherit, ObjectInherit','1',"Allow")
     
     $acl.SetAccessRule($AccessRule)
 
     $first,$last = $name -split " "
     $name = "$last $first"
     $acl | Set-Acl "..\$name"
+}
+
+#remove one user
+Foreach ($name in $foldernames) {
+    $acl=get-acl e:\"$name"
+    $first,$last = $name -split " "
+    $name = "$last $first"
+    $user = Get-ADUser -Filter "Name -eq '$name'" -Properties SamAccountName
+
+    $accessrule = New-Object system.security.AccessControl.FileSystemAccessRule("carmeuse\AWLONGWORTH","Read",,,"Allow")
+    $acl.RemoveAccessRuleAll($accessrule)
+
+    $first,$last = $name -split " "
+    $name = "$last $first"
+    Set-Acl -Path "e:\$name" -AclObject $acl
 }
